61a6e1d050319d6cc21c281fae98e10325450768
Shafiaqazi6 <shafiaqazi6@gmail.com>
2025-12-06 11:30:09 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+
+    socket.on("chatMessage", (msg) => {
+        console.log("Message:", msg);
+
+        // Sends message to all OTHER clients
+        socket.broadcast.emit("chatMessage", msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected', socket.id);
+    });
+});
+
+server.listen(4000, () => {
+    console.log('Server is running');
+});
